e8dc62cc3891a7283463e8fe1e69a0c352eeefa5
Drowsygoat_PDC <lecka48@liu.se>
2025-02-28 20:28:51 +0100

    fix space

---
+++ R_Scripts/performance_tester.R
@@ -0,0 +1,36 @@
+test_performance <- function(n, repetitions = 3) {
+  times <- numeric(repetitions)
+  
+  prime_factors <- function(num) {
+    factors <- c()
+    d <- 2
+    while (num > 1) {
+      while (num %% d == 0) {
+        factors <- c(factors, d)
+        num <- num / d
+      }
+      d <- d + 1
+      if (d * d > num && num > 1) {
+        factors <- c(factors, num)
+        break
+      }
+    }
+    return(factors)
+  }
+  
+  for (i in 1:repetitions) {
+    start_time <- Sys.time()
+    result <- prime_factors(n)
+    end_time <- Sys.time()
+    times[i] <- as.numeric(difftime(end_time, start_time, units = "secs"))
+  }
+  
+  avg_time <- mean(times)
+  
+  list(factors = result, avg_time_taken = avg_time)
+}
+
+# Example usage:
+n <- 635234937
+performance <- test_performance(n)
+print(performance)
